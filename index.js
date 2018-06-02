b498e0e1756bb2094ecee4ac5d5e3c5b253f3d24
Canino Developments <ian.canino@gmail.com>
2018-06-02 19:56:25 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+const employeeArray = ['Joe', 'Gabe'];
+function printBadges (employeeArray) {
+    for (let index = 0; index < employeeArray.length; index++) {
+        console.log(`Welcome ${employeeArray[index]}! You are employee #${index+1}.`);
+    }
+    return employeeArray;
+}
+
+function tailsNeverFails() {
+    let numTails = 0;
+    let toss = Math.random();
+    while (toss >= 0.5) {
+        numTails++;
+        toss = Math.random();
+
+    }
+    return `You got ${numTails} tails in a row!`;
+}
